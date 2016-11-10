b1a6cbed3be2f468c218082c7494e0f054eeb89e
Thomas Bridge <tbridge@chicagobooth.edu>
2016-11-10 13:50:32 +0000

    first thing

---
+++ argv.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+
+for($i = 0; $i<=$#ARGV; $i++) {
+	print "$i\t$ARGV[$i]\n";
+}
+
---
+++ get-as-macro-routes.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+use Net::IRR;
+use strict;
+
+my $DEBUG = 0;
+
+my $host = 'whois.radb.net';
+my $as_set = "AS-NETSOURCE";
+
+my $connection = Net::IRR->connect( host => $host) || die "Cannot connect to $host\n";
+
+if($ARGV[0]) {
+	$as_set = $ARGV[0];
+} 
+
+my @routes = get_routes_for_as_set($as_set);
+
+my $prefix_list_name = $as_set;
+my $route;
+
+foreach $route (sort @routes) {
+	print "ip prefix-list $prefix_list_name permit $route\n";
+} 
+
+
+sub get_routes_for_as_set {
+	my ($as_set) = @_;
+	my @aslist;
+		
+	if(!(@aslist = $connection->get_as_set($as_set, 1))) {
+		print STDERR "Looking up $as_set\n";
+		print STDERR "none found\n";
+		return undef;
+	}
+
+	my @routes;
+	my $as_num;
+	
+	foreach $as_num (@aslist) {
+		@routes = uniq(@routes, $connection->get_routes_by_origin($as_num));
+		if($DEBUG) {
+			print "After looking for $as_num we now have found $#routes\n";
+			print "@routes\n";
+		}
+	}
+
+	return(@routes);
+}
+
+sub uniq {
+	my %seen;
+	grep !$seen{$_}++, @_;
+}
+
